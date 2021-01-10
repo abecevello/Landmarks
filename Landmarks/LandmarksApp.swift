5bc3a5c3f56ce177a450676883c9a200e64a91c1
Adam Becevello <adam@becevello.ca>
2021-01-10 15:06:03 -0500

    SwiftUI Essentials: Creating and Combining Views

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Adam Becevello on 2021-01-10.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
